598a2cb951ee69b8bd1cc7c2860fc3be5295ae1b
Irem Gunay <iremguunay@gmail.com>
2022-02-06 14:02:29 +0300

    Added Bolum and Sehir pojo classes

---
+++ src/main/java/com/bilgeadam/personelapp/pojo/Bolum.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.personelapp.pojo;
+
+import lombok.Data;
+
+@Data
+public class Bolum {
+
+    private long no;
+    private String ad;
+    private long sehirNo;
+}
---
+++ src/main/java/com/bilgeadam/personelapp/pojo/Sehir.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.personelapp.pojo;
+
+import lombok.Data;
+
+@Data
+public class Sehir {
+
+    private long no;
+    private String ad;
+
+}
